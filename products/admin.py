2ee6613e983d913c8587444bf987b5576d6aab50
= <=>
2026-08-08 15:58:56 +0330

    creat admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'inventory', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['category', 'created_at']
+    search_fields = ['name', 'description']
+    list_editable = ['price', 'inventory']
