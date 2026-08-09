fd76cb07521cc7979258e7f394c5f05a0500a1da
= <=>
2026-08-09 18:08:15 +0330

    creat viewsets in view

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path , include
+from rest_framework.routers import DefaultRouter
+from .views import Product, Category, ProductViewSet, CategoryViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet , basename='products')
+router.register(r'categories', CategoryViewSet , basename='categories')
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Product,Category
+from .serializer import CategorySerializer,ProductSerializer
 # Create your views here.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
