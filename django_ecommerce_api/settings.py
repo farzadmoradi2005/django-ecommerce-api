59639439d3a23feb1e52bc9566e36694e5ed5b36
= <=>
2026-08-09 18:24:42 +0330

    creat pagination

--- django_ecommerce_api/settings.py
+++ django_ecommerce_api/settings.py
@@ -39,9 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
     'products',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- products/views.py
+++ products/views.py
@@ -1,11 +1,30 @@
+import django_filters
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets , filters
 from .models import Product,Category
+from django_filters.rest_framework import DjangoFilterBackend , FilterSet , NumberFilter
 from .serializer import CategorySerializer,ProductSerializer
 # Create your views here.
+class ProductFilter(FilterSet):
+    min_price = NumberFilter(field_name='price',lookup_expr='gte')
+    max_price = NumberFilter(field_name='price',lookup_expr='lte')
+    class Meta:
+        model = Product
+        fields = ['category','min_price','max_price']
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    # اتصال کلاس فیلتر سفارشی
+    filterset_class = ProductFilter
+
+    # فیلدهای قابل جستجوی متنی (شامل نام و توضیحات)
+    search_fields = ['name', 'description']
+
+    # فیلدهای قابل مرتب‌سازی
+    ordering_fields = ['price', 'created_at']
+    ordering = ['-created_at']  # پیش‌فرض: جدیدترین محصولات
